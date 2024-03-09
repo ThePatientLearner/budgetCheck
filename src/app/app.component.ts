184101afa90a6b4ed06ea2d94438577a5083144c
Roberto Casabán <Roberto.casaban@hotmail.com>
2024-03-09 23:13:48 +0100

    header and nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeadNavComponent } from './head-nav/head-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeadNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/head-nav/head-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-head-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './head-nav.component.html',
+  styleUrl: './head-nav.component.css'
+})
+export class HeadNavComponent {
+
+}
