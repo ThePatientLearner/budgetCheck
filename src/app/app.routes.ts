035c02180dbd064aa0b39d320af51199b2bd4582
Roberto Casabán <Roberto.casaban@hotmail.com>
2024-03-10 13:43:59 +0100

    initiate website with /expenditures path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,9 @@ import { Routes } from '@angular/router';
 import { ExpendituresComponent } from './expenditures/expenditures.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/Expenditures', pathMatch: 'full' }, // esto es para que inicie en esta ruta la pagina
     {
+        
         path: "Expenditures", component:ExpendituresComponent
     }
 ];
